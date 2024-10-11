0de87eed896d16015bc5c242a30359f220166703
Israel Estrada <israel@compilaideas.com>
2024-10-11 12:27:33 -0600

    Implementación de diseño de task list: Usando widget Card y provisionalmente una lista de tareas estática.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: SplashPage(),
+      home: const SplashPage(),
     );
   }
 }
--- lib/app/view/task_list/task_list_page.dart
+++ lib/app/view/task_list/task_list_page.dart
@@ -1,50 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:lista_tareas/app/model/task.dart';
 
-class TaskListPage extends StatefulWidget {
-  const TaskListPage({super.key});
+import '../components/h1.dart';
+import '../components/shape.dart';
 
-  @override
-  State<TaskListPage> createState() => _TaskListPageState();
-}
-
-class _TaskListPageState extends State<TaskListPage> {
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
+class TaskListPage extends StatelessWidget {
+  const TaskListPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Task List'),
-        leading: GestureDetector(
-          onTap: (){
-            Navigator.of(context).pop();
-          },
-          child: Center(child: Text('Atrás')),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.primary,
+            child: Column(
+              children: [
+                const Row(children: [Shape()]),
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/tasks-list-image.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                    const SizedBox(height: 16),
+                    const H1('Completa tus tareas', color: Colors.white),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left:30, right: 30, top: 35),
+            child: H1('Tareas'),
+          ),
+          Expanded( // ocupa el espacio height disponible
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: ListView.separated(
+                itemBuilder: (_, index) => _TaskItem(taskList[index]),
+                separatorBuilder: (_, __) => const SizedBox(height: 16,),
+                itemCount: taskList.length,),
+            ),
+          )
+        ],
       ),
-      body: Center(child: Text('Hola, el contador es $count'),),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          count++;
-          setState(() {
-
-          });
-        },
-        child: const Icon(Icons.add),
+        onPressed: () {},
+        child: const Icon(Icons.add, size: 50),
       ),
     );
   }
 }
+
+class _TaskItem extends StatelessWidget {
+  const _TaskItem(this.task, {Key? key}) : super(key: key);
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(21)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 18),
+        child: Row(
+          children: [
+            Icon(
+              Icons.check_box_outline_blank,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(width: 10),
+            Text(task.title),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final taskList = <Task> [
+   Task('Sacar al perro'),
+   Task('Sacar al perro'),
+   Task('Sacar al perro'),
+   Task('Sacar al perro'),
+   Task('Sacar al perro'),
+   Task('Sacar al perro'),
+];
