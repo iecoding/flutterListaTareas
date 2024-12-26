4399821f78a3852f7ac9a0ef0cb8c7ba8434f84f
Israel Estrada <israel@compilaideas.com>
2024-12-26 15:31:18 -0600

    Problema: The method 'watch' isn't defined for the type 'BuildContext'. (Documentation)  Try correcting the name to the name of an existing method, or defining a method named 'watch'.

--- lib/app/view/home/state_dif.dart
+++ lib/app/view/home/state_dif.dart
@@ -1,44 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart' show ChangeNotifierProvider, Provider;
 
-class StatePage extends StatefulWidget {
+class StatePage extends StatelessWidget {
   const StatePage({Key? key}) : super(key: key);
 
   @override
-  State<StatePage> createState() => _StatePageState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => EmailProvider(),
+      child: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _EmailTextField(),
+              SizedBox(height: 20),
+              _SendButton(),
+              SizedBox(height: 20,),
+              _EmailText()
+            ],
+          ),
+        )
+      ),
+    );
+  }
 }
 
-class _StatePageState extends State<StatePage> {
-  String email = '';
+class _EmailText extends StatelessWidget {
+  const _EmailText({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              onChanged: (value) => email = value,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                hintText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO
-              },
-              child: const Text('Enviar'),
-            )
-          ],
-        ),
-      )
+    final email = context.watch<EmailProvider>().email;
+    return Text('El email introducido es: $email');
+  }
+}
+
+class _SendButton extends StatelessWidget {
+  const _SendButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // TODO
+      },
+      child: const Text('Enviar'),
     );
+  }
+}
+
+class _EmailTextField extends StatelessWidget {
+  const _EmailTextField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) => Provider.of<EmailProvider>(context).email = value,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        hintText: 'Email',
+      ),
+    );
+  }
+}
+
+class EmailProvider extends ChangeNotifier {
+  String _email = '';
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+    notifyListeners();
   }
 }
